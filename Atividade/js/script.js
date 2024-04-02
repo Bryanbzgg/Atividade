898ea4c28ba8ee1e1fa3f11b2a0d0d22ca88f8ce
Bryan <seucleito123456@gmail.com>
2024-04-02 17:06:35 -0300

    02/04

--- Atividade/js/script.js
+++ Atividade/js/script.js
@@ -10,24 +10,136 @@ exercicio1 = () => {
 exercicio2 = () => {
   document.getElementById("resposta").innerHTML =
     "<h2>Resposta da Atividade</h2>";
-  var numero = document.getElementById("num").value;
-  var erro = document.getElementById("erro")
+  let numero = document.getElementById("num").value;
+  let erro = document.getElementById("erro");
   for (let i = 0; i <= numero; i += 2) {
-    document.getElementById("resposta").innerHTML += `#${i} `;
-
+    if (numero <= 0) {
+      erro.innerHTML = `digite um numero valido`;
+    } else {
+      document.getElementById("resposta").innerHTML += `#${i} `;
+    }
     console.log(i);
   }
 };
 
-exercicio3=()=>{
-    document.getElementById("resposta").innerHTML =
-    "<h2>Resposta da Atividade</h2>";
+exercicio3 = () => {
+  let num = document.getElementById("num").value;
+  let error = document.getElementById("erro");
+  let result = document.getElementById("resultado");
+  if (num <= 0) {
+    error.innerText = `o numero que foi digitado (${num}), não é valido!!`;
+    result.innerText = "";
+  } else {
+    result.innerText = "";
+    error.innerText = "";
+    let primo = true;
+    for (let i = 2; i < num; i++) {
+      if (num % i == 0) primo = false;
+    }
+    primo
+      ? (result.innerText = `o numero ${num} é primo`)
+      : (error.innerText = `o numero ${num} não é primo`);
+  }
+};
 
-    if(num / num == 0 && num / 1 == 0){
-        console.log(`é `)
-    }else{
+exercicio4 = () => {
+  let num = document.getElementById("num").value;
+  let error = document.getElementById("erro");
+  let result = document.getElementById("resultado");
+  error.innerText = "";
+  result.innerHTML = "";
+  if (num <= 0) {
+    error.innerText = `favor inserir numeros validos`;
+  } else {
+    for (let i = 1; i <= 10; i++) {
+      let x1 = num * i;
+
+      result.innerHTML += `${num} X ${i} = ${x1} <br>`;
+    }
+  }
+};
+
+exercicio5 = () => {
+
+  let numero = document.getElementById("num").value;
+  let erro = document.getElementById("erro");
+  let resul = document.getElementById("resultado");
+  let contador = 1;
+  resul.innerHTML = "";
+  erro.innerText = ""
+if(numero <= 0){
+  erro.innerText = `favor inserir numeros validos`;
+
+}else{
+
+  while (contador <= numero) {
+  
+    if (contador % 2 !== 0) {
+      resul.innerHTML += `${contador} <br>`;
+    }
+    
+    contador++;
+  }
+}
+
+  }
+
+  exercicio6 = () => {
+    let numero = document.getElementById("num").value;
+  let erro = document.getElementById("erro");
+  let resul = document.getElementById("resultado");
+  let contador = 0;
+  let somar = 0;
+
+  resul.innerHTML = "";
+  erro.innerText = ""
+if(numero <= 0){
+  erro.innerText = `favor inserir numeros validos`;
+
+}else{
+
+  while (contador <= numero) {
+  
+    if (contador % 2 == 0) {
+      somar += contador
+      // resul.innerHTML += `${contador} <br>`;
+      resul.innerHTML = `${somar } <br>`;
 
     }
-
-
+    
+    contador++;
+  }
 }
+
+  }
+
+  exercicio7 = () =>{
+let numero = 10
+let resul = document.getElementById("resposta");
+while(numero >= 1){
+ resul.innerHTML +=`${numero} <br>`;
+  console.log(numero)
+  numero--
+}
+  }
+  exercicio8 = () =>{
+
+  }
+
+  exercicio9 = () =>{
+
+let resul = document.getElementById("resposta");
+for (let i = 1; i <=100; i++) {
+ resul.innerHTML +=`${i} <br>`;
+  
+}
+  }
+
+  exercicio10 = () =>{
+
+    for (let i = 1; i <= 100; i++) {
+      
+    }
+
+    
+  }
